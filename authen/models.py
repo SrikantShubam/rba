900bc074a54e469c201a03a136062aab4130b349
Srikant Shubham <srikantsubham10000@gmail.com>
2022-08-27 17:20:27 +0530

    2nd I guess

---
+++ authen/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=100, blank=False)
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+
+class data_collected(models.Model):
+    userid=models.CharField(max_length=400,null=True,blank=True,default="something")
+    ip=models.CharField(max_length=40)
+    date=models.DateField()
+    time_zone=models.CharField(max_length=40,default='UTC',null=True,blank=True)
+    language=models.CharField(max_length=40,default='en-US',null=True,blank=True)
+    system_fonts=models.CharField(max_length=10000,default='Arial',null=True,blank=True)
+    browser_fonts=models.CharField(max_length=200,default='Arial',null=True,blank=True)
+    time_collected=models.TimeField(default=0)
+    city=models.CharField(max_length=20,default='SOME STRING',null=True,blank=True)
+    region=models.CharField(max_length=20,default='SOME STRING',null=True, blank=True)
+    country=models.CharField(max_length=40,default='SOME STRING',null=True, blank=True)
+    browser_name = models.CharField(max_length=400,default='Something')
+    browser_version = models.CharField(max_length=400,default='Something')
+    os_family = models.CharField(max_length=400,default="something")
+    os_version = models.CharField(max_length=400,default='Something')
+    screensize=models.CharField(max_length=20,default=0)
+    plugins=models.CharField(max_length=200,default='Something',null=True,blank=True)
+    canvas=models.CharField(max_length=200,default="null",null=True,blank=True)
+    webgl=models.CharField(max_length=200,default="null",null=True,blank=True)
+    ua_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    ip_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    timezone_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    location_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    screensize_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    system_fonts_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    browser_fonts_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    plugins_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    canvas_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    webgl_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    lang_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
+    overall_totaltime=models.CharField(max_length=200,default=0,blank=True,null=True)
---
+++ authen/views.py
@@ -0,0 +1,183 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
+from authen.models import data_collected
+
+from .forms import SignUpForm
+import time 
+import datetime
+from django.utils.timezone import make_aware
+import requests
+import ctypes
+def home(request):
+    try:
+        if request.user is not None:
+            total_start=time.time()
+            #username start--------------------
+            user_start=time.time()
+            print("User: ", request.user)
+            username=request.user
+            user_end=time.time()
+            totaltime_user=user_end-user_start
+    #username end-------------------------------------
+            #lang----------------start-----------
+            lang_start=time.time()
+            lang=request.META['HTTP_ACCEPT_LANGUAGE']
+            print(lang)
+            lang_end=time.time()
+            lang_totaltime=lang_end-lang_start
+             #-------------lang end---------------
+             #getting date and time ----------------------
+            naive_datetime = datetime.datetime.now()
+            naive_datetime.tzinfo  # None
+
+            # settings.TIME_ZONE  # 'UTC'
+            aware_datetime = make_aware(naive_datetime)
+            aware_datetime.tzinfo  # <UTC>
+
+
+            
+            start_timezone = time.time()
+
+            local_timezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+            print("Timezone of the user is --> ",local_timezone)
+            # printing date and time 
+            print("Date Time: ",aware_datetime)
+            print('date time -----',naive_datetime.date())
+            print('date time -----',naive_datetime.time())
+            time_collected=naive_datetime.time()   #time collected 
+            end_timezone = time.time()
+            final_timezone=end_timezone-start_timezone
+            print(final_timezone)
+
+            #timezone done------------------------
+
+
+             # getting ip-----------------------------------
+            start_ip = time.time()
+            response = requests.get('https://api64.ipify.org?format=json').json()
+            ip_address = response["ip"]
+            print('the ip address-------',ip_address)
+            end_ip = time.time()
+            final_ip = print(end_ip-start_ip)
+            #final ip=----------------
+
+
+
+            #location start ----------------------
+            # getting location from the ip 
+            response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+            location_start=time.time()
+                # "ip": ip_address,
+            city= response.get("city")
+            region=response.get("region")
+            country= response.get("country_name")
+            location_end=time.time()
+            location_totaltime=location_end-location_start
+            #location end-------------------------
+
+
+            #browser start----------------------------
+
+            ua_starttime = time.time()
+
+            browser_ua = request.user_agent.browser
+            system_ua = request.user_agent.os
+            ua_endtime = time.time()
+            ua_totaltime = ua_endtime - ua_starttime
+
+            print("ua----------- ",ua_totaltime)
+            #browser end----------------------
+            #screen size ---------------------------
+            screen_start=time.time()
+            user32 = ctypes.windll.user32
+            screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+            screen_end=time.time()
+            screen_totaltime=screen_end-screen_start
+            #screen size end ---------------------------
+
+
+
+            #system fonts start ---------------------
+            
+            import os  #for windows
+        
+            systemfonts_start=time.time()
+            from sys import platform
+            sys_fonts=[]
+            if platform == "win32":
+                sys_font=os.listdir((r'C:\Windows\fonts'))
+                sys_fonts=list(set(sys_font))
+                print("system fonts are ",sys_fonts)
+            elif platform =="macOS":
+                import pycocoa #for mac
+                manager = pycocoa.NSFontManager.sharedFontManager()
+                font_families = list(manager.availableFontFamilies())
+                print(font_families)
+            
+            else:
+                print("system fonts are not available")
+
+            systemfonts_final=time.time()
+            fonts_totaltime=systemfonts_final-systemfonts_start
+
+            #system fonts end ---------------------
+
+            
+            #browser fonts,canvas,webgl,plugins
+            import json
+            browser_fonts_start=time.time()
+            f=open(r"C:\Users\Srikant Shubham\Downloads\data.json")
+            plugins_start=time.time()
+            g=open(r"C:\Users\Srikant Shubham\Downloads\plugins.json")
+            h=open(r"C:\Users\Srikant Shubham\Downloads\canvas.json")
+            i=open(r"C:\Users\Srikant Shubham\Downloads\webgl.json")
+
+
+            browser_fonts = json.load(f)
+            browser_fonts_end=time.time()
+            browser_totaltime=browser_fonts_end-browser_fonts_start
+            plugins=json.load(g)
+            plugins_end=time.time()
+            plugins_totaltime=plugins_end-plugins_start
+            canvas_data=json.load(h)
+            canvas=canvas_data["canvas"]
+            canvas_totaltime=canvas_data["total_time"]
+        
+            
+            webgl_data=json.load(i)
+            webgl=webgl_data["webgl"]
+            webgl_totaltime=webgl_data["webgl_totaltime"]
+            print(plugins)
+            f.close()
+            g.close()
+            h.close()
+            i.close()
+            print(canvas,webgl)
+            os.remove(r'C:\Users\Srikant Shubham\Downloads\data.json')
+            os.remove(r'C:\Users\Srikant Shubham\Downloads\plugins.json')
+            os.remove(r"C:\Users\Srikant Shubham\Downloads\canvas.json")
+            os.remove(r"C:\Users\Srikant Shubham\Downloads\webgl.json")
+           
+            total_end=time.time()
+            overall_totaltime=total_end-total_start
+            data=data_collected(userid=username,ip=ip_address,browser_fonts=browser_fonts,system_fonts=sys_fonts,language=lang,time_zone =local_timezone,date=naive_datetime.date(),time_collected=time_collected,city=city,region=region,country=country,browser_name=browser_ua.family, browser_version =browser_ua.version_string,os_family=system_ua.family,os_version=system_ua.version_string,screensize=screensize,plugins=plugins,canvas=canvas,webgl=webgl,ua_totaltime=ua_totaltime,ip_totaltime=final_ip,timezone_totaltime=final_timezone,location_totaltime=location_totaltime,screensize_totaltime=screen_totaltime,system_fonts_totaltime=fonts_totaltime, browser_fonts_totaltime=browser_totaltime,plugins_totaltime=plugins_totaltime,canvas_totaltime=canvas_totaltime,webgl_totaltime=webgl_totaltime,lang_totaltime=lang_totaltime,overall_totaltime=overall_totaltime)
+            data.save()
+    except:
+        pass        
+    return render(request, 'auth/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'auth/signup.html', {'form': form})
