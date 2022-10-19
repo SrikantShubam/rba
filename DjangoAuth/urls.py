51385dec4135259d184e14c7860949561a48e92e
CHRIST-Infotech <infotech.lavasa@christuniversity.in>
2022-10-19 05:24:02 -0700

    updtes from server

--- DjangoAuth/urls.py
+++ DjangoAuth/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from django.contrib import admin
 from authen import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('admin/', admin.site.urls),
 ]
